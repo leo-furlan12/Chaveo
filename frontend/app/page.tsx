edf859bf33333191877c861632252ce54367a0a6
Leo <leo@leo-ubuntu.tail12e88d.ts.net>
2026-09-21 20:33:56 -0300

    feat: estrutura inicial da homepage (#3)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,69 +1,63 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Homepage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <header className="menu-topo">
+        <div className="nome">
+          <svg
+            className="icone-logo"
+            viewBox="0 0 120 120"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+            <path
+              d="M18 40 V22 a4 4 0 0 1 4 -4 H40"
+              stroke="#00D9B5"
+              strokeWidth="7"
+              strokeLinecap="round"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <path
+              d="M102 40 V22 a4 4 0 0 0 -4 -4 H80"
+              stroke="#00D9B5"
+              strokeWidth="7"
+              strokeLinecap="round"
+            />
+            <path
+              d="M18 80 V98 a4 4 0 0 0 4 4 H40"
+              stroke="#00D9B5"
+              strokeWidth="7"
+              strokeLinecap="round"
+            />
+            <path
+              d="M102 80 V98 a4 4 0 0 1 -4 4 H80"
+              stroke="#00D9B5"
+              strokeWidth="7"
+              strokeLinecap="round"
+            />
+            <circle cx="60" cy="52" r="14" fill="#00D9B5" />
+            <path d="M60 64 L50 86 H70 Z" fill="#00D9B5" />
+          </svg>
+          <h3>Chaveo</h3>
         </div>
+
+        <nav className="menu">
+          <a href="#" className="item-menu">Como Funciona?</a>
+          <a href="#" className="item-menu">Perguntas</a>
+          <a href="#" className="item-menu">Cadastra-se</a>
+          <a href="#" className="botao-login">Login</a>
+        </nav>
+      </header>
+
+      <main>
+
+        <section className="apresentacao">
+          <div className="texto-apresentacao">
+            <h1 className="titulo-principal">
+              O aluguel do seu jeito
+            </h1>
+          </div>
+        </section>
+       
       </main>
-    </div>
+    </>
   );
 }
